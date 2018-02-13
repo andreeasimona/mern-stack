a31e77ac6d3c231fb0e2475bbc875e1ec1657ccb
Simona Domnisoru <simona.domnisoru@gmail.com>
2018-02-13 15:51:39 +0100

    Project structure

---
+++ README.md
@@ -0,0 +1,4 @@
+Basic MERN application following this tutorial
+
+
+https://medium.com/@bryantheastronaut/react-getting-started-the-mern-stack-tutorial-feat-es6-de1a2886be50
---
+++ src/CommentBox.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class CommentBox extends Component {
+    constructor(props) {
+        super(props);
+    }
+    render() {
+        return (
+            <div> hello
+             </div>
+        )
+    }
+}
+export default CommentBox;
